b7238fcca7190b3a9beb8cb4950f45360d82d22f
LuisDuran-code <rd20014@ues.edu.sv>
2026-04-30 18:40:05 -0600

    Creacion de estructura del proyecto en Python

---
+++ Services/Menu_service.py
@@ -0,0 +1,11 @@
+class Menu:
+
+    def mostrar_menu(self):
+        print("================================")  
+        print("SISTEMA DE GESTIÓN DE EVENTOS")
+        print("==============================")
+        print("1. Crear Evento.")
+        print("2. Registrar Asistente.")
+        print("3. Mostrar Eventos.")
+        print("4. Salir")
+        print("==============================")
